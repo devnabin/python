8b18de0df1622086b7d823a6b2c93add338716ad
nabin <nabin123456j@gmail.com>
2024-10-20 14:43:09 +0545

    comment example

---
+++ chapter1/comment.py
@@ -0,0 +1,13 @@
+# print("hello comment")
+
+# we called pound can be achieved ctrl + / (fordward slash)
+
+"""
+Hello world, This is the comment example using double qoute
+"""
+'''
+Hello World, This is the comment example using single quote
+
+
+'''
+print("Hello world!")
