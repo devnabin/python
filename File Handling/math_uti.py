7247e1f7c7784200e5f6304da7011e820fe23d6e
Nabin Bhandari <nfornabin@gmail.com>
2025-11-09 17:14:25 -0600

    python learning

---
+++ File Handling/math_uti.py
@@ -0,0 +1,26 @@
+def add2number(a,b):
+    a = int(a)
+    b = int(b)
+    if(a != '' and b != ''):
+        return a+b
+    else:
+        return 'Number is missing or incorrect'
+    
+
+    
+
+def newFun(a,b):
+    return a+b
+
+
+def sub2number(a,b):
+    a = int(a)
+    b = int(b)
+    if(a != '' and b != ''):
+        return a-b
+    else:
+        return 'Number is missing or incorrect'
+
+
+
+nabin = "I am in ❤️ Python !"
